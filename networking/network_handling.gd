53f2a4b1aa3062bcc68e5f8c5e64e9445da112d1
LostPeon <1lost.peon@gmail.com>
2026-04-06 13:29:52 -0600

    Fixed the bad IP address

--- networking/network_handling.gd
+++ networking/network_handling.gd
@@ -7,19 +7,14 @@ const PORT: int = 42069
 var peer: ENetMultiplayerPeer
 
 func get_local_lan_ip() -> String:
-	for ip in IP.get_local_addresses():
-		# Only normal IPv4 LAN addresses
-		if ip.begins_with("192.168.") or ip.begins_with("10."):
-			return ip
+	var interfaces = IP.get_local_interfaces()
+	for iface in interfaces:
+		print(iface)
+		var friendly := str(iface.get("friendly", "")).to_lower()
+		var addresses = iface.get("addresses", [])
+		if "wi-fi" in friendly or "wifi" in friendly or "wlan" in friendly or "wireless" in friendly:
+			return addresses[1]
 		
-		# 172.16.0.0 - 172.31.255.255 are also private LAN addresses
-		if ip.begins_with("172."):
-			var parts := ip.split(".")
-			if parts.size() == 4:
-				var second_octet := int(parts[1])
-				if second_octet >= 16 and second_octet <= 31:
-					return ip
-	
 	return "127.0.0.1"
 
 func start_server() -> void:
