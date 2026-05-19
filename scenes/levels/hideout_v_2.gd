1a13e19fec7c0f4c9df4133366ba52214fdd7722
LostPeon <1lost.peon@gmail.com>
2026-05-19 16:50:04 -0600

    Grids

---
+++ scenes/levels/hideout_v_2.gd
@@ -0,0 +1,49 @@
+extends Level
+
+@export var grid_square_scene: PackedScene
+@export var grid_size: int = 10
+@export var tile_size: float = 1.0
+@export var grid_height: float = 0.52
+
+@onready var grid: Node3D = $Grid
+
+
+func _ready() -> void:
+	spawn_centered_grid(100)
+
+
+func spawn_grid(grid_count: int) -> void:
+	for x in range(grid_count):
+		for z in range(grid_count):
+			var square := grid_square_scene.instantiate() as Node3D
+			grid.add_child(square)
+
+			square.global_position = Vector3(
+				x * tile_size,
+				grid_height,
+				z * tile_size
+			)
+
+
+func spawn_centered_grid(grid_count: int) -> void:
+	var offset := (grid_count - 1) * tile_size * 0.5
+
+	for x in range(grid_count):
+		for z in range(grid_count):
+			var square := grid_square_scene.instantiate() as Node3D
+			grid.add_child(square)
+
+			square.global_position = Vector3(
+				(x * tile_size) - offset,
+				grid_height,
+				(z * tile_size) - offset
+			)
+
+
+
+func spawn_grid_square(pos: Vector3) -> void:
+	var square := grid_square_scene.instantiate()
+	square.global_position = pos
+	add_child(square)
+
+# square.global_position = Vector3(x, 0.02, z)
